4c64f9acbb01d6ecf14aabc0716457f955ba98fc
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-02-05 22:49:26 -0500

    creacion de frontend

--- backend/app/routes/recommendations.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/recommendations", summary="Obtener recomendaciones", description="Este endpoint devuelve recomendaciones personalizadas para los estudiantes.")
-async def get_recommendations():
-    return {"message": "Aquí van las recomendaciones"}
