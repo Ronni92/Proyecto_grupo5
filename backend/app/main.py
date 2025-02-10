6d295e11f7cb268efbb49e06b1719f7227a77fed
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-02-09 23:12:58 -0500

    Correcion de archivos html

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from flask import Flask
+from flask import send_from_directory
 
 # Agregar el directorio actual al path de Python
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -9,6 +10,10 @@ from routes import routes_bp  # Ahora Flask podrá encontrar el módulo
 
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/assets')
 app.register_blueprint(routes_bp)
+
+@app.route('/assets/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('../frontend/assets',filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
